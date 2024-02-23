c9374cfaac52aecd27c37de5282348ea17364708
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-02-23 20:43:11 +0100

    Add files via upload

---
+++ Ejemplos.java
@@ -0,0 +1,13 @@
+package sentencias;
+
+public class Ejemplos {
+    public static void main(String[]args){
+        String name = "Homer Simpson";
+        String dni = "39900754";
+        String emptyString = "";
+        String nullString = null;
+
+        System.out.println("Longitud de la cadena:" + name.length());
+        
+    }
+}
